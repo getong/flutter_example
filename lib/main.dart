6e17c9e8c55b744dc963b747b44509b88b36df76
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-02-17 00:40:43 +0800

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,115 @@
 import 'package:flutter/material.dart';
-import 'package:phoenix_wings/phoenix_wings.dart';
-import 'package:audioplayers/audio_cache.dart';
-import 'dart:math';
 
-// copy from https://elixirforum.com/t/how-to-integrate-flutter-with-phoenix/29819/8
-
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mimitos!',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Mimitos!'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
-  final socket = PhoenixSocket("ws://192.168.1.106:4000/socket/websocket");
-  final AudioCache player = new AudioCache();
-  final purrAudioPath = "purr.mp3";
-  final faces = ["😫", "😩", "😣", "😔", "😑", "😌", "😊"];
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  PhoenixChannel _channel;
-  int _happyness;
+  int _counter = 0;
 
-  void initState() {
-    _happyness = 0;
-    _connectSocket();
-    super.initState();
-  }
-
-  Future<void> _connectSocket() async {
-    await widget.socket.connect();
-
-    _channel = widget.socket.channel("room:mimitos");
-    _channel.on("mimitos!", _incrementHappyness);
-
-    _channel.join();
-  }
-
-  void _incrementHappyness(_payload, _ref, _joinRef) {
-    widget.player.play(widget.purrAudioPath);
-
-    if (_happyness < 26) {
-      setState(() {
-        _happyness = _happyness + 1;
-      });
-    } else {
-      setState(() {
-        var rng = new Random();
-        _happyness = rng.nextInt(10) + 5;
-      });
-    }
-  }
-
-  void _sendMimitos() {
-    _channel.push(event: "send_mimitos!", payload: {});
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-          child: Container(
-              alignment: Alignment.center,
-              color: Colors.deepPurple,
-              child: Text(widget.faces[_happyness ~/ 4],
-                  style: TextStyle(
-                      fontFamily: 'Raleway',
-                      fontSize: 156,
-                      decoration: TextDecoration.none,
-                      color: Colors.white)))),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _sendMimitos,
-        tooltip: 'Send Mimitos!',
-        child: new Icon(Icons.pets),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
